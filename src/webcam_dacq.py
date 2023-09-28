33f8c86242fd3a2eb340956964431370b7fa5095
grasseau <grasseau@subatech.in2p3.fr>
2023-09-28 14:08:49 +0200

    fete de la science 2023

--- src/webcam_dacq.py
+++ src/webcam_dacq.py
@@ -1,8 +1,11 @@
 import cv2
 import time
 
+storeImage = False
+waitTime = 0.2  # Sleep in sec.
 
-camera = cv2.VideoCapture(0)
+# camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(2)
 camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 nextImage = True
 sumAcquisition=0.
@@ -13,22 +16,23 @@ sumTotal=0.
 n=0
 while(nextImage):
   # Image acquisition
-  sAcquisition = time.perf_counter_ns()
+  sAcquisition = time.perf_counter()
   print("Start image ", n)
   fileName = "DACQ/img_{}.jpeg".format(n)
   ret,frame = camera.read()
-  eAcquisition = time.perf_counter_ns()
+  eAcquisition = time.perf_counter()
 
   # Processing & Show image
   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
   # cv2.imshow('Capturing Video',frame)
   cv2.imshow('Capturing Video',gray)
-  time.sleep(0.5)
-  eProcessing = time.perf_counter_ns()
+  time.sleep(waitTime)
+  eProcessing = time.perf_counter()
   
   # Store
-  cv2.imwrite(fileName, gray)
-  eStore = time.perf_counter_ns()
+  if storeImage: 
+    cv2.imwrite(fileName, gray)
+  eStore = time.perf_counter()
   
   # Continue or not
   # Wait a key stroke in ms
@@ -37,12 +41,12 @@ while(nextImage):
     nextImage = False
   
   # Display in ms
-  dtAcquisition = (eAcquisition - sAcquisition)* 1.e-6
-  dtComputing = (eProcessing - eAcquisition)* 1.e-6
-  dtStore = (eStore - eProcessing)* 1.e-6
-  dtTotal = (eStore - sAcquisition)* 1.e-6
+  dtAcquisition = (eAcquisition - sAcquisition) # * 1.e-6
+  dtComputing = (eProcessing - eAcquisition) # * 1.e-6
+  dtStore = (eStore - eProcessing)  # * 1.e-6
+  dtTotal = (eStore - sAcquisition) # * 1.e-6
   print("  Total time={:.2f}, dacq={:.2f}, processing={:.2f}, storage={:.2f} in ms".format(dtTotal, dtAcquisition, dtComputing, dtStore) )
-  print("  Rate={:.1f} fps".format( 1000./ (dtTotal) ))
+  print("  Rate={:.1f} fps".format( 1./ (dtTotal) ))
   sumAcquisition += dtAcquisition
   sumComputing += dtComputing
   sumStore += dtStore
@@ -53,7 +57,7 @@ print("-----------------------------------------------------------")
 print("Averages on {} images:".format(n))
 print("  Total time={:.2f}, dacq={:.2f}, processing={:.2f}, storage={:.2f} in ms".format(
        n, sumTotal/n, sumAcquisition/n, sumComputing/n, sumStore/n) )
-print("  Rate={} fps".format( (1000.0*n)/ sumTotal ))
+print("  Rate={} fps".format( (1.0*n)/ sumTotal ))
 
 # Terminate
 camera.release()
